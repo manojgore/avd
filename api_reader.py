b3ce3e381559f310384d778e5fbb3df6c9d3dc2e
Manoj Gore <manojgore.mg@gmail.com>
2025-10-25 19:37:20 +0530

    testing

--- api_reader.py
+++ api_reader.py
@@ -1,15 +1,20 @@
 import requests
 import pandas as pd
+import os
 
-# Extract: Read data from a public API
-response = requests.get("https://jsonplaceholder.typicode.com/users")
-data = response.json()
+token = os.getenv("API_Token")
+print(f'Token : {token}')
 
-# Transform: Convert to DataFrame and select key fields
-df = pd.DataFrame(data)
-df = df[["id", "name", "email", "company"]]
-df["company"] = df["company"].apply(lambda x: x["name"])
 
-# Load: Display transformed result
-print("Transformed User Data:")
-print(df)
+# # Extract: Read data from a public API
+# response = requests.get("https://jsonplaceholder.typicode.com/users")
+# data = response.json()
+
+# # Transform: Convert to DataFrame and select key fields
+# df = pd.DataFrame(data)
+# df = df[["id", "name", "email", "company"]]
+# df["company"] = df["company"].apply(lambda x: x["name"])
+
+# # Load: Display transformed result
+# print("Transformed User Data:")
+# print(df)
